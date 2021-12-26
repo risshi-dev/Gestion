3452b486c9d5a0b28ce1add681319a782767fcd2
Sandeep Mandal <2019ucp1423@mnit.ac.in>
2021-12-26 13:31:55 +0530

    Added landing illustration

--- components/Landing/Landing.js
+++ components/Landing/Landing.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
 
 export default function Landing() {
   return (
@@ -9,31 +9,30 @@ export default function Landing() {
         <meta name="description" content="A Project Management Tool" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <div className={styles.title}>
-          Gestion
-        </div>
+        <div className={styles.title}>Gestion</div>
       </main>
+      <div className={styles.innerContainer}>
+        <div className={styles.content}>
+          <div className={styles.tagLine}>
+            Make Your <b>110%</b> of
+            <br />
+            <b>Everyday</b>
+          </div>
 
-      <div className={styles.tagLine}>
-          Make Your <b>110%</b> of<br/>
-          <b>Everyday</b>
-      </div>
+          <div className={styles.invite}>
+            <div>Invite Your Team Members</div>
 
-      <div className={styles.Invite}>
-        <div>
-            Invite Your Team Members
+            <form className={styles.form}>
+              <input type="text" placeholder="email" />
+              <button type="submit"> Invite </button>
+            </form>
+          </div>
         </div>
-
-        <form className={styles.form}>
-          <input type='text' placeholder='email'/>
-          <button type='submit'> Invite </button>
-        </form>
-          
+        <div className={styles.image}>
+          <img src="/landing_illustration.svg" alt="landing illustration" />
+        </div>
       </div>
-
-      
     </div>
-  )
+  );
 }
