4dc753a38f180602ccb3f89d6a95d99ef2e71109
Anurag-ji <trivedianurag619@gmail.com>
2022-01-19 17:32:25 +0530

    firebase setup

--- components/Chat/SendMessage.js
+++ components/Chat/SendMessage.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import Chat from "../../styles/Chats.module.css";
+import { collection } from "firebase/firestore/lite";
+import db from "./firebase";
+
 function SendMessage() {
+
+const uid = 123;
+const displayName = 'Anurag';
+const dummyspace = useRef();
+const [newMessage, setNewMessage] = useState("");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+  collection(db, 'messages')
+    .add({
+      text: newMessage,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      displayName,
+    });
+
+    setNewMessage("");
+
+    dummyspace.current.scrollIntoView({behavor: "smooth"});
+};
+
   return (
     <div>
+      <section ref={dummyspace}></section>
+      <form onSubmit={handleSubmit}>
       <input
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
         className={Chat.chatInput}
         type="text"
         placeholder="share your thoughts"
       />
-      <button className={Chat.chatButton}>Send </button>
+      <button className={Chat.chatButton} type="submit" disabled={!newMessage}>Send </button>
+      </form>
     </div>
   );
 }
