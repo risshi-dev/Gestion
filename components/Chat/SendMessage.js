bdb155e684c65c6f9031c564dcd0e22cbf12ef3b
risshi-dev <2019ucp1415@mnit.ac.in>
2022-01-19 17:30:55 +0530

    firebase intialization for chat in projects

--- components/Chat/SendMessage.js
+++ components/Chat/SendMessage.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Chat from "../../styles/Chats.module.css";
+import db from "./firebase";
+import firebase from "firebase";
 function SendMessage() {
+  const uid = 123;
+  const displayName = "Anurag";
+  const dummyspace = useRef();
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection("messages").add({
+      text: newMessage,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      displayName,
+    });
+
+    setNewMessage("");
+
+    dummyspace.current.scrollIntoView({ behavor: "smooth" });
+  };
+
   return (
     <div>
-      <input
-        className={Chat.chatInput}
-        type="text"
-        placeholder="share your thoughts"
-      />
-      <button className={Chat.chatButton}>Send </button>
+      <section ref={dummyspace}></section>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className={Chat.chatInput}
+          type="text"
+          placeholder="share your thoughts"
+        />
+        <button
+          className={Chat.chatButton}
+          type="submit"
+          disabled={!newMessage}
+        >
+          Send{" "}
+        </button>
+      </form>
     </div>
   );
 }
