8cbb75db50e50256fb969c50a7ac62d48ff19fea
Sandeep Mandal <2019ucp1423@mnit.ac.in>
2022-01-20 13:03:16 +0530

    added cors library for cors errors

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ import projectRoute from "./routes/ProjectRoutes.js";
 import cardRoute from "./routes/CardRoutes.js";
 
 const app = express();
+app.use(cors());
+
 dotenv.config();
 
 await mongooseConnect();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use("/", (req, res, next) => {
   console.log("API is running...🤩");
